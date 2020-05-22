08158556481d5d093232c444bc0b3fa1d641872a
András Jankó <dandrid2@gmail.com>
2020-05-22 21:35:43 +0200

    added built application

---
+++ build/precache-manifest.e9d3ca0035598de2d47e0e9194251f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e241b4f8bd2bd20329daf0df50e382a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "660375a0455fc0804801",
+    "url": "./static/css/main.1f2c742c.chunk.css"
+  },
+  {
+    "revision": "07c155597dc075058377",
+    "url": "./static/js/2.821ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.821ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660375a0455fc0804801",
+    "url": "./static/js/main.35f4254c.chunk.js"
+  },
+  {
+    "revision": "763f551d24bf50b1938c",
+    "url": "./static/js/runtime-main.c7b6dbd4.js"
+  }
+]);
